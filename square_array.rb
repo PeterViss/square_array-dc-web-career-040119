e123bd4f225fba5b92d69ed9bf59d706df64005c
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-22 05:36:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
-def square_array(array)
-  number = [1, 2, 3]
-
-  array.each do |number|
-    puts number.push ** 2
-
-  end
-number
-     # your code here
+new_array = []
+ array.each do |x|
+   new_array.push x*x
+end
+new_array
+end your code here
 end
